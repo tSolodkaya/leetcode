93b042024fd2c60882be4476c99eef2a183ef2c2
Tetiana Solodka <solodkaygit@gmail.com>
2024-02-28 20:33:11 +0100

    add task contains duplicates 2

---
+++ js/219-contains-duplicates-2.js
@@ -0,0 +1,39 @@
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+
+// Example 2:
+
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+
+// Example 3:
+
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
+var containsNearbyDuplicate = function (nums, k) {
+  if (nums.length === 1) {
+    return false;
+  }
+
+  for (let i = 0; i < nums.length; i += 1) {
+    for (let j = i + 1; j < nums.length; j += 1) {
+      if (nums[i] === nums[j]) {
+        if (j - i <= k) {
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
+};
+
+console.log(containsNearbyDuplicate([-1, -1], 1));
+
+console.log(containsNearbyDuplicate([1, 2, 3, 1], 3));
+console.log(containsNearbyDuplicate([1, 0, 1, 1], 1));
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2));
